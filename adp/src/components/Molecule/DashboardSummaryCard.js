4647be070c158c7213bda702801ab5d31dbe5ade
chittu2605 <chittaranjan.ghoshal@gmail.com>
2021-08-01 15:33:15 +0530

    share values highlited

--- adp/src/components/Molecule/DashboardSummaryCard.js
+++ adp/src/components/Molecule/DashboardSummaryCard.js
@@ -175,21 +175,21 @@ class DashboardSummaryCard extends React.Component {
 
 Your Business Stats till date is as follows:
                     
-Current Month Repurchase (PBV) : ${pbv ? pbv : 0} (Ideal 2000 to 6000 PBV)
+Current Month Repurchase (PBV) : *${pbv ? pbv : 0}* (Ideal 2000 to 6000 PBV)
                     
-Total PBV till date : ${totalPbv ? totalPbv : 0} 
+Total PBV till date : *${totalPbv ? totalPbv : 0}*
                     
-Current Month GBV : ${gbv}
+Current Month GBV : *${gbv}*
                     
-GBV till date : ${totalGbv}
+GBV till date : *${totalGbv}*
                     
-Current Month BV : ${bv}
+Current Month BV : *${bv}*
 
-Total BV Till Date : ${bvTillDate}
+Total BV Till Date : *${bvTillDate}*
                     
-Your Current Zone : ${zone} (${zoneValue}%)
+Your Current Zone : *${zone} (${zoneValue}%)*
                     
-Personal New Joining till date : ${noCoSponsored}
+Personal New Joining till date : *${noCoSponsored}*
                     
                     
 Wish you success!!!`,
